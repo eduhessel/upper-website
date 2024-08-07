9890590211fa2b73668bd31ea1e50f5019dd742a
Eduardo Hessel <edu.hessel@hotmail.com>
2024-08-07 11:37:36 -0300

    feat: add new section experiencia transparente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import styles from '@/app/styles/home.module.css';
-import { InformationCircleIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { AcademicCapIcon, ChartBarIcon, CurrencyDollarIcon, InformationCircleIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
@@ -153,6 +153,44 @@ export default function Home() {
               <span>Nossa missão é democratizar o acesso a serviços financeiros e educação de qualidade. Queremos que cada pessoa, independentemente de sua experiência ou conhecimento prévio, tenha a oportunidade de investir de forma inteligente e segura, e adquirir as habilidades necessárias para prosperar financeiramente.</span>
             </div>
           </div>
+        </motion.div>
+      </section>
+      <section className={styles.sectionExperienciaTransparente}>
+        <motion.div className={styles.containerSectionExperienciaTransparente}
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}>
+          <h2>Uma experiência transparente</h2>
+          <div className={styles.containerCardExperienciaTransparente}>
+            <div className={styles.cardExperienciaTransparenteContent}>
+              <div className='w-[80px] h-[80px] bg-transparent border-white border-solid border rounded-full flex justify-center items-center'>
+                <CurrencyDollarIcon className='max-w-[56px] text-white stroke-1'/>
+              </div>
+              <div className='flex flex-col text-white gap-2'>
+                <p className='font-normal'>Serviços Bancários Completos</p>
+                <span>Com a nossa conta digital, você pode realizar todas as suas transações financeiras de maneira simples e segura. Desde pagamentos e transferências até gerenciamento de contas, nossa plataforma está preparada para atender todas as suas necessidades bancárias diárias. Venha ser Upper agora mesmo!</span>
+              </div>
+            </div>
+            <div className={styles.cardExperienciaTransparenteContent}>
+              <div className='w-[80px] h-[80px] bg-transparent border-white border-solid border rounded-full flex justify-center items-center'>
+                <ChartBarIcon className='max-w-[56px] text-white stroke-1'/>
+              </div>
+              <div className='flex flex-col text-white gap-2'>
+                <p className='font-normal'>Investimentos Diversificados</p>
+                <span>Oferecemos uma wallet de investimentos robusta, onde você pode diversificar seu portfólio com criptomoedas, CCBs e NCs. Nossa plataforma é projetada para ser intuitiva e acessível, permitindo que tanto investidores iniciantes quanto experientes possam investir com confiança.</span>
+              </div>
+            </div>
+            <div className={styles.cardExperienciaTransparenteContent}>
+              <div className='w-[80px] h-[80px] bg-transparent border-white border-solid border rounded-full flex justify-center items-center'>
+                <AcademicCapIcon className='max-w-[56px] text-white stroke-1'/>
+              </div>
+              <div className='flex flex-col text-white gap-2'>
+                <p className='font-normal'>Educação Financeira</p>
+                <span>Acreditamos que o conhecimento é a chave para o sucesso financeiro. Por isso, temos uma parceria exclusiva com a Cebrac, uma das principais plataformas de cursos profissionalizantes do Brasil. Através dessa parceria, nossos clientes têm acesso a uma ampla variedade de cursos e recursos educacionais.</span>
+              </div>
+            </div>
+          </div>
+          <button className={styles.primaryButton}>ABRIR CONTA</button>
         </motion.div>
       </section>
     </main>
